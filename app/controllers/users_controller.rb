70a3c4de132243d073d443020b16c2d06a92200e
Nidhin Nambiar <dnidhin5@gmail.com>
2015-09-08 15:45:02 +0530

    added user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,31 @@ class UsersController < ApplicationController
     end
 
   end
+
+  def edit
+    if current_user
+      @user = User.find(params[:id])
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  def update
+    if current_user
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    else
+      redirect_to log_in_path
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :name, :mob, :age)
   end
 end
